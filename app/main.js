e31f17d5c9d917d132f3985adcba73511de64115
Darryl <darrylbayliss@dazindustries.co.uk>
2016-03-25 21:42:56 +0000

    Position window under tray item.

--- app/main.js
+++ app/main.js
@@ -17,6 +17,7 @@
 'use strict';
 
 const electron = require('electron');
+const positioner = require('electron-positioner')
 const argv = require('yargs').argv;
 const fs = require('fs');
 
@@ -27,13 +28,15 @@ const UPDATE_FEED_URL = 'http://roman-update-service.appspot.com/s/check_updates
 
 const DEV_MODE = argv.dev;
 
-
-let isTrayMode = true;
 let mainWindow;
+let mainWindowPositioner;
 let trayIcon;
 
-
 app.on('ready', () => {
+
+  trayIcon = new electron.Tray(__dirname + '/assets/TrayIconTemplate.png');
+  trayIcon.setToolTip(app.getName());
+
   mainWindow = new electron.BrowserWindow({
     width: 200,
     height: 12 * 2 /* padding */
@@ -41,22 +44,26 @@ app.on('ready', () => {
           + (32 + 2) * 14 /* 14 values */,
     resizable: false,
     skipTaskbar: true,
+    movable: false,
     maximizable: false,
     fullscreenable: false,
     frame: false
   });
 
+  mainWindow['hide']();
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.showInactive();
-  updateUiMode();
+  mainWindow.on('closed', () => mainWindow = null);
+
+  mainWindowPositioner = new positioner(mainWindow);
 
   if (DEV_MODE) {
     mainWindow.webContents.openDevTools({ detach: true });
   }
 
-  mainWindow.on('show', () => updateUiMode());
-  mainWindow.on('hide', () => updateUiMode());
-  mainWindow.on('closed', () => mainWindow = null);
+  trayIcon.on('click', function(event, bounds) {
+    mainWindow[mainWindow.isVisible() ? 'hide' : 'show']();
+    mainWindowPositioner.move('trayCenter', bounds);
+  })
 
   if (!DEV_MODE) {
     checkForAppUpdates();
@@ -65,102 +72,6 @@ app.on('ready', () => {
 
 
 app.on('window-all-closed', () => app.quit());
-
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-  if (!mainWindow.isVisible()) {
-    mainWindow.show();
-  }
-});
-
-
-electron.ipcMain.on('update-ui-mode', updateUiMode);
-
-
-function updateUiMode() {
-  // Build or teardown tray
-  if (isTrayMode) {
-    if (!trayIcon) {
-      trayIcon = new electron.Tray(__dirname + '/assets/TrayIconTemplate.png');
-      trayIcon.setToolTip(app.getName());
-    }
-  } else if (trayIcon) {
-    trayIcon.destroy();
-    trayIcon = null;
-  }
-
-  // Build or teardown dock
-  if (!isTrayMode) {
-    app.dock.show();
-  } else {
-    app.dock.hide();
-  }
-
-  // Update menu
-  var menuItems = [];
-
-  if (isTrayMode) {
-    menuItems.push({
-      label: mainWindow.isVisible() ? 'Hide Colors' : 'Show Colors',
-      click: () => {
-        mainWindow[mainWindow.isVisible() ? 'hide' : 'show']();
-        updateUiMode();
-      }
-    });
-  }
-
-  menuItems.push({
-    label: isTrayMode ? 'Switch to Normal App Mode' : 'Switch to Menu Bar Mode',
-    click: () => {
-      isTrayMode = !isTrayMode;
-      updateUiMode();
-    }
-  });
-
-  menuItems.push({ type: 'separator' });
-  menuItems.push({ label: 'About ' + app.getName(), role: 'about' });
-
-  if (isTrayMode) {
-    menuItems.push({ label: 'Quit', click: () => app.quit() });
-  }
-
-  var contextMenu = electron.Menu.buildFromTemplate(menuItems);
-
-  if (isTrayMode) {
-    trayIcon.setContextMenu(contextMenu);
-    mainWindow.setAlwaysOnTop(true);
-  } else {
-    app.dock.setMenu(contextMenu);
-    mainWindow.setAlwaysOnTop(false);
-  }
-
-  //electron.Menu.setApplicationMenu(contextMenu);
-  writePrefs();
-}
-
-
-function readPrefs() {
-  try {
-    var prefStr = fs.readFileSync(app.getPath('userData') + '/prefs.json');
-    if (prefStr) {
-      var json = JSON.parse(prefStr);
-      isTrayMode = !!json.isTrayMode;
-    }
-  } catch (e) {}
-}
-
-
-function writePrefs() {
-  fs.writeFileSync(app.getPath('userData') + '/prefs.json', JSON.stringify({
-    isTrayMode: isTrayMode
-  }));
-}
-
 
 function checkForAppUpdates() {
   let packageInfo = require('./package.json');
@@ -185,6 +96,3 @@ function checkForAppUpdates() {
   });
   autoUpdater.checkForUpdates();
 }
-
-
-readPrefs();
