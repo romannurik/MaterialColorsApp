ae287bd74dec8ee4adf2a0c5ebe90837fdc7d331
Roman Nurik <roman@nurik.net>
2016-05-19 18:43:28 -0700

    tweak search presentation

--- app/app.js
+++ app/app.js
@@ -89,7 +89,6 @@ class MaterialColors {
 
   _buildUi() {
     let firstHue;
-    let greyColor = this.COLORS['grey'];
 
     let $searchButton = $('<div>')
         .addClass(`${this.CLASS_NAMES.searchButton}`)
@@ -98,7 +97,10 @@ class MaterialColors {
 
     let $searchIcon = $('<div>')
         .addClass(this.CLASS_NAMES.searchIcon)
-        .css('background-color', greyColor['500'])
+        .append($(`
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+          </svg>`))
         .appendTo($searchButton);
 
     $('<div>')
@@ -174,9 +176,9 @@ class MaterialColors {
       // help text
       this.$searchHelpText = $('<div>')
           .addClass(this.CLASS_NAMES.searchHelpText)
-          .text('Search by material color name or code. \
-                 Copy any color code format to clipboard \
-                 to detect the color name.')
+          .text(`Search by material color name or hex value.
+                 Copy any color code format to the clipboard
+                 to detect the color name.`)
           .appendTo(this.$searchResults);
 
       $searchInput.focus();
