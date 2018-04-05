9d276067e419d8bb41cfad5a8907842a6cb40af5
Roman Nurik <roman@nurik.net>
2018-04-05 15:13:05 -0400

    More build fixes

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ const electronPackager = require('electron-packager');
 const electronOsxSign = require('electron-osx-sign');
 const argv = require('yargs').argv;
 const fs = require('fs');
+const archiver = require('archiver');
 const {execSync} = require('child_process');
 
 const colorsFile = argv.colorsFile;
@@ -105,19 +106,19 @@ gulp.task('run-electron', ['build'], $.shell.task([
 gulp.task('dist', ['build', 'install-packages'], function(cb) {
   let packageInfo = require('./build/package.json');
   electronPackager({
-    'arch': 'x64',
-    'dir': 'build',
-    'out': 'dist',
-    'asar': true,
-    'platform': 'darwin',
-    'prune': false, // seems to be a bug where not just devDependencies but also dependencies is pruned
-                    // we don't need pruning anyway since we do npm install --production
-    'app-bundle-id': packageInfo.appBundleId,
-    'app-category-type': 'app-category-type=public.app-category.developer-tools',
-    'app-version': packageInfo.version,
-    'icon': iconFile || 'icon.icns',
-    'name': packageInfo.appDisplayName,
-    'overwrite': true,
+    arch: 'x64',
+    dir: 'build',
+    out: 'dist',
+    asar: true,
+    platform: 'darwin',
+    prune: false, // seems to be a bug where not just devDependencies but also dependencies is pruned
+                  // we don't need pruning anyway since we do npm install --production
+    appBundleId: packageInfo.appBundleId,
+    appCategoryType: 'app-category-type=public.app-category.developer-tools',
+    appVersion: packageInfo.version,
+    icon: iconFile || 'icon.icns',
+    name: packageInfo.appDisplayName,
+    overwrite: true,
   }, (err, appPaths) => {
     let appPath = appPaths[0];
     if (!appPath) {
@@ -151,7 +152,15 @@ gulp.task('dist', ['build', 'install-packages'], function(cb) {
           throw new Error(`Error signing the app bundle: ${err}`);
         }
 
-        cb();
+        // zip up the directory
+        console.log('Zipping up the package');
+        let zipStream = fs.createWriteStream(`./dist/${packageInfo.version}.zip`)
+            .on('error', err => { throw err })
+            .on('end', () => cb());
+        let archive = archiver('zip', {zlib: {level: 9}})
+        archive.pipe(zipStream);
+        archive.directory(appFilePath, `${packageInfo.appDisplayName}.app`);
+        archive.finalize();
       });
     });
   });
